c521b1fe989d95f72f4363d4a4ad94218b6de308
Ahmed M samir <ahmed2017170572@cis.asu.edu.eg>
2024-06-07 04:41:54 +0200

    Add project files.

---
+++ ClientAPI/Program.cs
@@ -0,0 +1,55 @@
+
+using ClientAPI.Services;
+using static GRPS_Test.Protos.InventoryService;
+
+namespace ClientAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddScoped<IApiKeyProviderService, ApiKeyProviderService>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddGrpcClient<InventoryServiceClient>(o =>
+            {
+                o.Address = new Uri("https://localhost:7153");
+            }).AddCallCredentials((context, metadata, serviceProvider) =>
+            {
+                var apiKeyProvider = serviceProvider.GetRequiredService<IApiKeyProviderService>();
+                var apiKey = apiKeyProvider.GetApiKey();
+                metadata.Add("X-Api-Key", apiKey);
+                return Task.CompletedTask;
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ ServerAPI/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authentication;
+using ServerAPI.Handlers;
+using ServerAPI.Services;
+
+namespace ServerAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddGrpc();
+
+            builder.Services.AddScoped<IApiKeyAuthenticationService, ApiKeyAuthenticationService>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "ApiKey";
+            }).AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>("ApiKey", configureOptions => { });
+
+            builder.Services.AddAuthorization();
+
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapGrpcService<MessageInventoryService>();
+
+
+            app.Run();
+        }
+    }
+}
